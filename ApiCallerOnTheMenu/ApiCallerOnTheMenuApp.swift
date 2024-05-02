2b26f3a9790cedbba5d372844bd2bc9703970937
Kim <kxmliebl@gmail.com>
2024-05-02 14:33:27 +0200

    Initial Commit

---
+++ ApiCallerOnTheMenu/ApiCallerOnTheMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCallerOnTheMenuApp.swift
+//  ApiCallerOnTheMenu
+//
+//  Created by K. Liebl on 02.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallerOnTheMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
